528b9a1cdf686699d82b55ba0240ef07d553f8fe
joaorezends <joaocarlosrezendejunior@gmail.com>
2025-01-02 11:10:50 -0300

    add handlebars

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,23 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
+  app.useStaticAssets({
+    root: join(__dirname, '..', 'public'),
+    prefix: '/public/',
+  });
+  app.setViewEngine({
+    engine: {
+      handlebars: require('handlebars'),
+    },
+    templates: join(__dirname, '..', 'views'),
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
