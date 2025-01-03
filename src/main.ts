95a97d969be0cbbdd8739a981312f38c423e66d3
joaorezends <joaocarlosrezendejunior@gmail.com>
2025-01-03 03:14:39 -0300

    adjust localhost

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
     new FastifyAdapter({ logger: true }),
   );
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
